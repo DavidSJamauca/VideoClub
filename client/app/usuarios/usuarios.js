f40e50a651264ab45125927042ff8dfe35c768fa
J_David_Jimenez <Jdceron2@misena.edu.co>
2017-10-21 14:19:14 -0500

    Hola :v

--- client/app/usuarios/usuarios.js
+++ client/app/usuarios/usuarios.js
@@ -1,18 +1,20 @@
 'use strict';
 
 angular.module('videoClubApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('usuarios-list', {
-        url: '/usuarios-list',
-        template: '<usuarios-list></usuarios-list>'
-      })
-      .state('usuarios-create', {
-        url: '/usuarios-create',
-        template: '<usuarios-create></usuarios-create>'
-      })
-      .state('usuarios-update', {
-        url: '/usuarios-update/:idUsuario',
-        template: '<usuarios-update></usuarios-update>'
-      });
-  });
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('usuarios-list', {
+                url: '/usuarios-list',
+                authenticate: true,
+                template: '<usuarios-list></usuarios-list>'
+            })
+            .state('usuarios-create', {
+                url: '/usuarios-create',
+                authenticate: ['ADMIN', 'VENDEDOR'],
+                template: '<usuarios-create></usuarios-create>'
+            })
+            .state('usuarios-update', {
+                url: '/usuarios-update/:idUsuario',
+                template: '<usuarios-update></usuarios-update>'
+            });
+    });
--- client/auth/auth.service.js
+++ client/auth/auth.service.js
@@ -1,78 +1,84 @@
 /*
-* Auth Service es el Service que gestiona el proceso de
-* Longin de la aplicacion
-*/
-
-
-
+ * Auth Service es el Service que gestiona el proceso de
+ * Longin de la aplicacion
+ */
 'use strict';
 
-function AuthService($auth,$state){
-  var Auth = {
-    login: login,
-    logout: logout,
-    isAuthenticated: isAuthenticated,
-    isAdmin: isAdmin,
-    isUser: isUser
-  };
+function AuthService($auth, $state) {
+    var Auth = {
+        login: login,
+        logout: logout,
+        isAuthenticated: isAuthenticated,
+        isAdmin: isAdmin,
+        isUser: isUser,
+        getRoles: getRoles
+    };
 
-  function login(user){
-    $auth.login(user)
-    .then(response => {
-      $state.go("main");
-      console.log("Login Realizado correctamente");
-    })
-    .catch(err => {
-      $state.go("login");
-      console.log("Error en el login");
-    })
-  }
-
-  function logout(){
-    if(Auth.isAuthenticated()){
-      $auth.logout()
-      .then(response => {
-        $state.go("main");
-        console.log("Salida OK");
-      })
+    function login(user) {
+        $auth.login(user)
+            .then(response => {
+                $state.go("main");
+                console.log("Login Realizado correctamente");
+            })
+            .catch(err => {
+                $state.go("login");
+                console.log("Error en el login");
+            })
     }
-  }
 
-  function isAuthenticated(){
-    if($auth.isAuthenticated()){
-      return true;
-    } else {
-      return false;
+    function logout() {
+        if (Auth.isAuthenticated()) {
+            $auth.logout()
+                .then(response => {
+                    $state.go("main");
+                    console.log("Salida OK");
+                })
+        }
     }
-  }
 
-  function isAdmin(){
-    if(Auth.isAuthenticated()){
-      if($auth.getPayload().roles.indexOf("ADMIN") !== -1){
-        return true;
-      }else{
-        return false;
-      }
-    }else{
-      return false;
+    function isAuthenticated() {
+        if ($auth.isAuthenticated()) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 
-  function isUser(){
-    if(Auth.isAuthenticated()){
-      console.log($auth.getPayload().roles);
-      if($auth.getPayload().roles.indexOf("USER") !== -1){
-        return true;
-      }else{
-        return false;
-      }
-    }else{
-      return false;
+    function getRoles() {
+        if (Auth.isAuthenticated()) {
+            return $auth.getPayload().roles
+        } else {
+            return false;
+        }
     }
-  }
 
-  return Auth;
-}//Final Function AuthService
+    function isAdmin() {
+        if (Auth.isAuthenticated()) {
+            if ($auth.getPayload().roles.indexOf("ADMIN") !== -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    function isUser() {
+        if (Auth.isAuthenticated()) {
+            console.log($auth.getPayload().roles);
+            if ($auth.getPayload().roles.indexOf("USER") !== -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return Auth;
+} //Final Function AuthService
 
 angular.module("videoClubApp")
-.factory("AuthService",AuthService);
+    .factory("AuthService", AuthService);
